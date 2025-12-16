040a012703987bd440f88992dfe008250c11d1b8
jmjohnson3 <70002823+jmjohnson3@users.noreply.github.com>
2025-12-16 12:23:32 -0700

    Gate OddsPortal debug overrides behind explicit flag

--- NFL_SPORTSBETTING.py
+++ NFL_SPORTSBETTING.py
@@ -1274,6 +1274,7 @@ class OddsPortalFetcher:
         self._auto_debug_remaining = 0
         self._html_override_path: Optional[Path] = None
         self._override_only = env_flag("NFL_ODDSPORTAL_OVERRIDE_ONLY", False)
+        self._use_debug_overrides = env_flag("NFL_ODDSPORTAL_USE_DEBUG_OVERRIDE", False)
         debug_png_env = os.environ.get("NFL_ODDSPORTAL_DEBUG_PNG")
         self._debug_png_enabled = str(debug_png_env or "").strip().lower() in {
             "1",
@@ -1476,7 +1477,7 @@ class OddsPortalFetcher:
 
         if self._html_override_path is not None:
             search_paths.append(self._html_override_path)
-        else:
+        elif self._use_debug_overrides:
             if self._debug_dir is not None:
                 search_paths.append(self._debug_dir)
             default_debug_dir = SCRIPT_ROOT / "reports" / "oddsportal_debug"
